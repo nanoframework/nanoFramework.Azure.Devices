31a765c0b8e08f958a1b91b566b9792e6ecb7a99
José Simões <jose.simoes@eclo.solutions>
2021-06-28 08:33:30 +0100

    Fix properties

--- Azure.Devices.DeviceClient/Properties/AssemblyInfo.cs
+++ Azure.Devices.DeviceClient/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("nanoFramework.Azure.Devices")]
+[assembly: AssemblyTitle("nanoFramework.Azure.Devices.Client")]
 [assembly: AssemblyCompany("nanoFramework Contributors")]
-[assembly: AssemblyProduct("nanoFramework.Azure.Devices")]
+[assembly: AssemblyProduct("nanoFramework.Azure.Devices.Client")]
 [assembly: AssemblyCopyright("Copyright (c) .NET Foundation and Contributors")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
