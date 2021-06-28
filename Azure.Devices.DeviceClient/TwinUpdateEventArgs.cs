af11d15d4bafb9fa634279ddfa02e710bd9f4d6f
José Simões <jose.simoes@eclo.solutions>
2021-06-28 08:38:07 +0100

    Add missing using

--- Azure.Devices.DeviceClient/TwinUpdateEventArgs.cs
+++ Azure.Devices.DeviceClient/TwinUpdateEventArgs.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using nanoFramework.Azure.Devices.Client;
 using System;
 
 namespace nanoFramework.Azure.Devices.Shared
