b090cad38151f99e7e432e36808d8248fc64644b
Laurent Ellerbach <laurelle@microsoft.com>
2021-08-01 18:13:11 +0200

    Adding Device Provisioning Service in Azure IoT SDK

---
+++ Azure.Devices.DeviceClient/RegistrationOperationStatus.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace nanoFramework.Azure.Devices.Provisioning
+{
+    /// <summary>
+    /// Registration operation status.
+    /// </summary>
+    internal class RegistrationOperationStatus
+    {
+        public const string Assigned = "assigned";
+        public const string Assigning = "assigning";
+        public const string Unassigned = "unassigned";
+        public const string Disabled = "disabled";
+        public const string Failed = "failed";
+
+        public RegistrationOperationStatus()
+        { }
+
+        /// <summary>
+        /// Gets or sets operation Id.
+        /// </summary>
+        public string operationId { get; set; }
+
+        /// <summary>
+        /// Gets or sets device enrollment status. Possible values include:
+        /// 'unassigned', 'assigning', 'assigned', 'failed', 'disabled'
+        /// </summary>
+        public string status { get; set; }
+
+        /// <summary>
+        /// The registration state
+        /// </summary>
+        public RegistrationState registrationState { get; set; }
+    }
+
+    internal class RegistrationOperationStatusSimple
+    {
+        public RegistrationOperationStatusSimple()
+        { }
+
+        /// <summary>
+        /// Gets or sets operation Id.
+        /// </summary>
+        public string operationId { get; set; }
+
+        /// <summary>
+        /// Gets or sets device enrollment status. Possible values include:
+        /// 'unassigned', 'assigning', 'assigned', 'failed', 'disabled'
+        /// </summary>
+        public string status { get; set; }
+    }
+}
---
+++ Azure.Devices.DeviceClient/RegistrationState.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+
+namespace nanoFramework.Azure.Devices.Provisioning
+{
+    /// <summary>
+    /// Used for the deserialization of the registration message
+    /// </summary>
+    internal class RegistrationState
+    {
+        public string registrationId { get; set; }
+
+        public DateTime createdDateTimeUtc { get; set; }
+
+        public string assignedHub { get; set; }
+
+        public string deviceId { get; set; }
+
+        public string status { get; set; }
+
+        public string substatus { get; set; }
+
+        public DateTime lastUpdatedDateTimeUtc { get; set; }
+
+        public string etag { get; set; }
+
+        public int errorCode { get; set; }
+
+        public string errorMessage { get; set; }
+
+        public string payload { get; set; }
+    }
+
+}
