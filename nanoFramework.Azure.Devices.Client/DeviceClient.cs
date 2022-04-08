51dbc8b1fe7a361068c8ed51c5ac9698a583dd2b
José Simões <jose.simoes@eclo.solutions>
2022-04-08 12:50:09 +0100

    Add telemetry data (#153)
    ***NO_CI***

--- nanoFramework.Azure.Devices.Client/DeviceClient.cs
+++ nanoFramework.Azure.Devices.Client/DeviceClient.cs
@@ -160,8 +160,15 @@ namespace nanoFramework.Azure.Devices.Client
             if (!string.IsNullOrEmpty(ModelId))
             {
                 userName += $"&model-id={HttpUtility.UrlEncode(ModelId)}";
-
             }
+
+            // compose product info
+            string productInfo = "nano;";
+            productInfo += $"azrsdk{ThisAssembly.AssemblyInformationalVersion};";
+            productInfo += $"{Runtime.Native.SystemInfo.TargetName}";
+
+            // add to user name
+            userName += $"&DeviceClientType={HttpUtility.UrlEncode(productInfo)}";
 
             // Now connect the device
             string key = _isCertificate ? _privateKey : Helper.GetSharedAccessSignature(null, _sasKey, $"{_iotHubName}/devices/{_deviceId}", new TimeSpan(24, 0, 0));
