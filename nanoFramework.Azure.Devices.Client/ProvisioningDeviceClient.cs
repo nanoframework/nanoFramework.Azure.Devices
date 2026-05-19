becfcdb794ea8ed896810e8fa6928ba16f2cf539
José Simões <jose.simoes@eclo.solutions>
2026-05-19 10:49:13 +0100

    Fix GC API  call

--- nanoFramework.Azure.Devices.Client/ProvisioningDeviceClient.cs
+++ nanoFramework.Azure.Devices.Client/ProvisioningDeviceClient.cs
@@ -212,7 +212,7 @@ namespace nanoFramework.Azure.Devices.Provisioning.Client
             while ((!cancellationToken.IsCancellationRequested) && (_status != ProvisioningRegistrationStatusType.Assigned))
             {
                 // We force to clean the memory at this stage, for constrained devices
-                GC.Run(true);
+                GC.Collect();
                 Thread.Sleep(200);
                 // Don't ask for a new message if we are already processing one
                 if ((_retry > 0) && (!_isMessageProcessed))
@@ -229,10 +229,10 @@ namespace nanoFramework.Azure.Devices.Provisioning.Client
 
             // Creating a DeviceRegistrationResult only when it's needed
             // One more time cleaning the memory for small devices, a bit of performances penalties
-            GC.Run(true);
+            GC.Collect();
             if (!string.IsNullOrEmpty(_message))
             {
-                var opeStat = (RegistrationOperationStatus)nanoFramework.Json.JsonConvert.DeserializeObject(_message, typeof(RegistrationOperationStatus));
+                var opeStat = (RegistrationOperationStatus)Json.JsonConvert.DeserializeObject(_message, typeof(RegistrationOperationStatus));
                 var reg = opeStat.registrationState;
                 var status = AssignStatus(opeStat.status);
                 if (_statusLast == ProvisioningRegistrationStatusType.Assigned)
